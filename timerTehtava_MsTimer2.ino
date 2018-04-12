905cfda7793a1eda521d6c29136aed5bf0a15076
Jani Palo-oja <34990455+janipalooja@users.noreply.github.com>
2018-04-12 23:26:09 +0300

    Create timerTehtava_MsTimer2.ino

---
+++ timerTehtava_MsTimer2.ino
@@ -0,0 +1,37 @@
+#include <TimeLib.h>
+#include <MsTimer2.h>
+
+int hhmmss = 0;
+
+void printTime() {
+
+    char buffer [10];
+
+    /*
+     * sprintf()
+     * 
+     * Write formatted data to string
+     * Composes a string with the same text that would be printed if format was used on printf, but instead of being printed, the content is stored as a C string in the buffer
+     * http://www.cplusplus.com/reference/cstdio/sprintf/
+    */
+    hhmmss = sprintf (buffer, "%02d:%02d:%02d", hour(), minute(), second());
+
+    for(int l = 0; l <= hhmmss; l++) {
+      Serial.print(buffer[l]);
+      }
+      Serial.println();
+}
+
+void setup() {
+  Serial.begin(9600);
+
+  // tunnit, minuutit, sekunnit, päivä, kuukausi, vuosi
+  setTime(23, 30, 00, 12, 04, 2018);
+    
+  MsTimer2::set(1000, printTime);
+  MsTimer2::start();
+}
+
+void loop() {
+  // Silence is Golden :)
+}
